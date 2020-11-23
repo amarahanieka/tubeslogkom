c51d89d88c19136923cdb91c495ce32f07a91ca2
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2020-11-23 13:40:17 +0700

    Update battle.pl

--- battle.pl
+++ battle.pl
@@ -1,11 +1,11 @@
 /* attack(senjata yang dipake, HP akhir enemy) --> mengattack enemy */
-attack(A,B) :-
-    enemy(_,_,_,X),
-    senjata(A,A1),
-    Z1 is X-A1,
-    identitas(_,_,_,W,_,_),
-    B1 is Z1-W,
-    enemy(_,_,Y,_),
-    B is B1+Y,
+attack(Senjata,B) :-
+    enemy(_,_,_,HP),
+    senjata(Senjata,AttSen),
+    HP1 is HP-AttSen,
+    identitas(_,_,_,Attack,_,_),
+    B1 is HP1-Attack,
+    enemy(_,_,Defense,_),
+    B is B1+Defense,
     enemy(_,_,_,B). 
     /* masih ngeluarin false */
