c3c5c50933d9e79ec7186c72d7c8bae85c965705
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2020-11-23 10:52:33 +0700

    items

---
+++ items.pl
@@ -0,0 +1,26 @@
+/** Senjata (senjata,attack) **/
+senjata(woodensword,0).
+senjata(rocksword,5).
+senjata(ironsword,10).
+senjata(diamondsword,20).
+
+senjata(woodenbow,0).
+senjata(rockbow,5).
+senjata(ironbow,10).
+senjata(diamondbow,20).
+
+senjata(magicbook,0).
+senjata(elderwand,5).
+senjata(elementbending,10).
+senjata(mindbending,20).
+
+/** Armor (armor,defense) **/
+armor(leather,5).
+armor(iron,10).
+
+/** Asesori (asesori,attack tambahan) **/
+asesori(swordenhancer,5).
+asesori(eagleseye,5).
+asesori(amulet,5).
+
+/** Potion **/
