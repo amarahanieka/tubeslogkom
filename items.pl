2d310a5f4cca1e9cf6eb5a7d7f5623db42863c1e
Amara Hanieka <68474184+amarahanieka@users.noreply.github.com>
2020-11-25 20:09:56 +0700

    Add files via upload

--- items.pl
+++ items.pl
@@ -1,18 +1,18 @@
-/** Senjata (senjata,attack,jenis) **/
-senjata(woodensword,0,senjata).
-senjata(rocksword,5,senjata).
-senjata(ironsword,10,senjata).
-senjata(diamondsword,20,senjata).
+/** Senjata (senjata,attack,jenis,owner) **/
+senjata(woodensword,0,senjata,swordsman).
+senjata(rocksword,5,senjata,swordsman).
+senjata(ironsword,10,senjata,swordsman).
+senjata(diamondsword,20,senjata,swordsman).
 
-senjata(woodenbow,0,senjata).
-senjata(rockbow,5,senjata).
-senjata(ironbow,10,senjata).
-senjata(diamondbow,20,senjata).
+senjata(woodenbow,0,senjata,archer).
+senjata(rockbow,5,senjata,archer).
+senjata(ironbow,10,senjata,archer).
+senjata(diamondbow,20,senjata,archer).
 
-senjata(magicbook,0,senjata).
-senjata(elderwand,5,senjata).
-senjata(elementbending,10,senjata).
-senjata(mindbending,20,senjata).
+senjata(magicbook,0,senjata,sorcerer).
+senjata(elderwand,5,senjata,sorcerer).
+senjata(elementbending,10,senjata,sorcerer).
+senjata(mindbending,20,senjata,sorcerer).
 
 /** Armor (armor,defense,jenis) **/
 armor(leather,5,armor).
@@ -29,3 +29,6 @@ potion(patronus,potion).
 potion(powerplus,potion).
 potion(heal,potion).
 potion(doubleheal,potion).
+
+/* lain-lain wakwkakw */
+    
