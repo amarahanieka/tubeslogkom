9b2cd00fb77705f1d750d1658c0df6b73e5da33b
aisyahfarrasaqila <aisyahfarrasaqila@gmail.com>
2020-11-25 21:33:50 +0700

    Update battle.pl

--- battle.pl
+++ battle.pl
@@ -1,11 +1,22 @@
 /* attack(senjata yang dipake, HP akhir enemy) --> mengattack enemy */
-attack(Senjata,B) :-
+attack(Senjata) :-
     enemy(_,_,_,HP),
     senjata(Senjata,AttSen,Jenis,Owner),
     HP1 is HP-AttSen,
-    identitas(_,_,_,Attack,_,_),
-    B1 is HP1-Attack,
-    enemy(_,_,Defense,_),
-    B is B1+Defense,
+    identitas(_,_,_,AttackP,_,_),
+    B1 is HP1-AttackP,
+    enemy(_,_,DefenseE,_),
+    B is B1+DefenseE,
+    retract(enemy(Musuh,Attack,Defense,_)),
+    asserta(enemy(Musuh,Attack,Defense,B)).
+
+specialattack(Senjata) :-
+    enemy(_,_,_,HP),
+    senjata(Senjata,AttSen,Jenis,Owner),
+    HP1 is HP-AttSen,
+    identitas(_,_,_,AttackP,_,_),
+    B1 is HP1-AttackP,
+    enemy(_,_,DefenseE,_),
+    B is B1+DefenseE,
     retract(enemy(Musuh,Attack,Defense,_)),
     asserta(enemy(Musuh,Attack,Defense,B)).
