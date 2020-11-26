ff7a833c47e5cfce14dd93ffdadc0d89ff845690
amarahanieka <68474184+amarahanieka@users.noreply.github.com>
2020-11-26 22:19:16 +0700

    leveling broooOOOo

--- leveling.pl
+++ leveling.pl
@@ -1,9 +1,28 @@
-naiklevel :-
-    identitas(Job, Level, Attack, Defense, MaxHP, Gold),
+/* buat naik level */
+naiklevel(Level) :-
+    identitas(Job, Level, Attack, Defense, EXP, HP, Gold),
+    EXP >= 500,
     Level = 1,
     LevelBaru is (Level+1),
     AttackBaru is (Attack+35),
     DefenseBaru is (Defense+20),
-    MaxHPBaru is (MaxHP+400),
-    retract(identitas(Job, Level, _, _, _, _)),
-    asserta(identitas(Job, LevelBaru, AttackBaru, DefenseBaru, MaxHPBaru, Gold)).
+    HPBaru is 400,
+    retract(identitas(Job, Level, _, _, _, _, _)),
+    asserta(identitas(Job, LevelBaru, AttackBaru, DefenseBaru, EXP, HPBaru, Gold)).
+
+naiklevel(Level) :-
+    identitas(Job, Level, Attack, Defense, EXP, HP, Gold),
+    EXP >= 750,
+    Level = 2,
+    LevelBaru is (Level+1),
+    AttackBaru is (Attack+35),
+    DefenseBaru is (Defense+20),
+    HPBaru is 1000,
+    retract(identitas(Job, Level, _, _, _, _, _)),
+    asserta(identitas(Job, LevelBaru, AttackBaru, DefenseBaru, EXP, HPBaru, Gold)).
+
+/* setiap kali naik level baru, HP nya otomatis jadi max hp di level tersebut ok mantap 
+nilai attack itu tetep (abis battle jg tetep)
+nilai defense juga tetep (abis battle jg tetep) 
+EXP ga berubah, jadi kalo sebelum level up 521, setelah level up juga tetep 521 ga ngulang dari awal */
+
